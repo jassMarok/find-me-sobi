6031e7d4fe57e261fa2d884beaa186a6ff693305
Jaspal <jasskot18@gmail.com>
2017-11-28 22:04:39 -0500

    Loosely working solution

---
+++ components/php/read-data-json.php
@@ -0,0 +1,15 @@
+<?php
+$csv = array_map('str_getcsv', file('SOBI_HUBS.csv'));
+array_walk($csv, function(&$a) use ($csv) {
+  $a = array_combine($csv[0], $a);
+});
+array_shift($csv); # remove column header
+
+
+echo json_encode($csv);
+
+/*Working Returning Json data from a file in same directory need to figure out how to open file streams from 
+other directories.
+*/
+
+?>
