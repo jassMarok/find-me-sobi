ce14d0bc4a4da5c1716cd5a9ee134c5a591aea78
Jaspal <jasskot18@gmail.com>
2017-12-05 21:52:22 -0500

    Working compute distance with static data

--- _/js/listingPageScript.js
+++ _/js/listingPageScript.js
@@ -13,14 +13,22 @@ function getJson(){
     });
 
 }
-function addHubMarkers(thisJson){                                        //Add SoBI hubs markers on map
-    var parsedJson=JSON.parse(thisJson);
-    for (var i = 0; i < parsedJson.length; i++) {  
+function addHubMarkers(tempJson){                                        //Add SoBI hubs markers on map
+    var thisJson=JSON.parse(tempJson);
+    for (var i = 0; i < thisJson.length; i++) {  
 
         marker = new google.maps.Marker({
-        position: new google.maps.LatLng(parsedJson[i]["LATITUDE"],parsedJson[i]["LONGITUDE"]),
+        position: new google.maps.LatLng(thisJson[i]["LATITUDE"],thisJson[i]["LONGITUDE"]),
         map: map
       });
 }
-findNearest(parsedJson);
+findNearest(thisJson);
+}
+/* Use this library to load geometry
+<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=geometry">
+</script> */
+function findNearest(tempJson){                                        //Find nearest point on map
+    var thisJson=tempJson;
+    
+
 }
